075372709bf92b762070ce8b8c5ce58b5174c1a3
Christian Ruiz <cruiz@uccs.edu>
2024-01-27 18:03:55 -0700

    UI there, Hold down LClick and wiggle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.ProBuilder.MeshOperations;
@@ -9,11 +10,11 @@ public class Player : MonoBehaviour
 {
     private CharacterController characterController;
     private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimation && characterController.isGrounded;
-    private bool ShouldTickle => Input.GetMouseButtonDown(0) && closeToEnemy == true;
+    private bool ShouldTickle => Input.GetMouseButton(0) && closeToEnemy == true;
 
     [Header("Functional Options")]
     [SerializeField] private bool canCrouch = true;
-    [SerializeField] private bool tickleMode = true;
+    [SerializeField] private bool tickleMode = false;
 
     [Header("Controls")]
     [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
@@ -48,6 +49,10 @@ public class Player : MonoBehaviour
     Vector2 mouseMovementOld = Vector2.zero;
     Vector2 mouseMovementNew = Vector2.zero;
     int count = 0;
+
+    [SerializeField] public UIScript laughBar;
+    public int maxLaughter = 10;
+    public int currentLaughter;
     
 
     private void Awake()
@@ -59,7 +64,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentLaughter = 0;
+        laughBar.SetMaxLaughter(maxLaughter);
     }
 
     
@@ -69,13 +75,14 @@ public class Player : MonoBehaviour
             {
             HandleCrouch();
             }
-       //if (tickleMode == true)
-            //{
+        Debug.Log("Shouldtickle: " + ShouldTickle.ToString());
+       if (ShouldTickle)
+            {
             Tickle();
-            //}
+            }
         //Debug.Log("Tickle Meter: " + tickleMeter);
     }
-
+    
 
     private void HandleCrouch()
     {
@@ -115,44 +122,44 @@ public class Player : MonoBehaviour
 
     private void HandleTickle()
     {
-       //if (ShouldTickle)
-        //{
+       if (ShouldTickle)
+        {
             Tickle();
-        //}
+        }
     }
 
     private void Tickle()
     {
-        //Debug.Log("*************************************NEW FRAME*************************************");
         if (count == 0)
         {
             currentFramePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
+           // Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
         }
         else if (count == 10)
         {
             oneFrameOldPoint = currentFramePoint;
             currentFramePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
+            //Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
         }
         else if (count == 20)
         {
             twoFrameOldPoint = oneFrameOldPoint;
             oneFrameOldPoint = currentFramePoint;
             currentFramePoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
+            //Debug.Log("Count: " + count.ToString() + "\n" + "Current Frame Point: " + currentFramePoint.ToString() + "\n" + "One Frame Behind: " + oneFrameOldPoint.ToString() + "\n" + "Two Frames Behind: " + twoFrameOldPoint.ToString() + "\n");
             mouseMovementOld = oneFrameOldPoint - twoFrameOldPoint;
             mouseMovementNew = currentFramePoint - oneFrameOldPoint;
             float movementDot = Vector2.Dot(mouseMovementOld, mouseMovementNew);
-            Debug.Log("\nDot Product: " + movementDot.ToString());
+            //Debug.Log("\nDot Product: " + movementDot.ToString());
             if (movementDot < 0)
             {
-                tickleMeter += 10;
-                Debug.Log("\nAdded to Tickle Meter");
+                currentLaughter += 1;
+                laughBar.SetLaughter(currentLaughter);
+                //Debug.Log("\nAdded to Tickle Meter");
             }
             else if (movementDot >= 0)
             {
-                Debug.Log("\nNO ADDITION");
+                //Debug.Log("\nNO ADDITION");
             }
         }
         if (count >= 30)
